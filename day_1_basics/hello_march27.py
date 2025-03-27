21a14a0210b72ebfa7193506b67ae2ad4a6ed3cf
Valdis Saulespurens <38938337+ValRCS@users.noreply.github.com>
2025-03-27 21:35:53 +0200

    Add files via upload

---
+++ day_1_basics/hello_march27.py
@@ -0,0 +1,83 @@
+# In Python we use hashmark for comments
+# Comments are for humans , computers ignore comments
+# Comments should explain WHY
+# when learning we can also write WHAT
+print("Hello March 27th Pythonists!") # I can add comment here too
+# print works on any text - Latvian, Japanese, emojis any UTF-8
+print("Pasmaidīsim! 😃")
+
+# let's try to break things
+print(" Kur tu teci Gailīti manu? 🐔")
+
+# i can use single quotes - in Python they work identically
+print('Single quotes are just like double quotes')
+
+# i can show one type of quotes inside text by using others outside
+print('Alice said "follow the white rabbit" and fell into a hole')
+
+# we can use Python as calculators
+print(2+2)
+# different from
+print("2+2") # this is text not calculation
+# we could print both
+print("2+2=",2+2) # comma by default gives us whitespace when printing
+# instead since Python 3.6 some 7 years ago
+# we can use so called f-strings
+print(f"Sooo 2+2={2+2} indeed") # what is inside curly bracers gets evaluated
+# compare without f 
+print("Sooo 2+2={2+2} indeed") # probably not what you want
+
+# subtraction
+print(10-3)
+print(f"10-3={10-3}")
+
+# multiply is with *
+print(5*6)
+
+# division is a bit trickier
+print(10/2) # gives us floating point values (have . not ,)
+# what happens 10/3
+print(10/3)
+print(round(10/3)) # rounds to nearest integer
+print(round(10/3, 4)) # rounds 4 digits after comma
+
+# we also have whole division
+print(10//3) # we use //
+print(10//6) # should be 1 (we have reminder of 4)
+
+# we can also show reminder / sort of similar to modulo
+print(10%6)
+print(10%2, 11%2, 12%2, 13%2)
+
+# we have usual parenthesis for precedence
+print((2+5)*(3+7))
+
+# we have built in power
+print("2**3 is 2 cubed", 2**3)
+print("2**8 is 2 to the power of 8", 2**8)
+
+# how about 2 bytes (16bits)
+print("2**16 is", 2**16)
+
+# for example we have RGB with 8 bits for each  so 24 bits
+print("2**24 is ", 2**24)
+
+print("2**32 is", 2**32)
+
+# to break through 4GB limit we use 64 bit systems
+print("2**64 is", 2**64)
+
+# Python supports Big Numbers by default automatically
+print("What is one Googol?", 10**100)
+
+# how about strings and operations
+# we can concatenate strings with +
+print("Valdis " + "likes Python")
+
+# i can not do this
+# print("Valdis" + 2025) # we can not add string and integer
+
+# however I can multiply string with a number!
+print("Beer 🍺 " * 5)
+# could be useful for dividers
+print("*"*40)
