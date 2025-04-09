759ca4f015e2adc3be86492ed9ae1ac09cadb790
Valdis Saulespurens <valdis.s.coding@gmail.com>
2025-04-09 18:36:30 +0300

    list stub

---
+++ topic_6_lists/lists_m26.py
@@ -0,0 +1,34 @@
+# So we know our basic primitive data types
+# we have int, float, str, bool
+# but we also have a more complex data type called a list
+
+# why should we need a list?
+# let's see we want to store some numbers
+a1 = 1
+a2 = 2
+a3 = 3
+a4 = 5
+a5 = 8
+a6 = 13
+# what do I do if I need to store 100 ? or 1 million numbers?
+
+# to solve this Python  offers a list data type
+# a list is a collection of items, a sequence of items
+# a list can contain any data type, including other lists
+# best use of lists is to store a collection of items of the same type
+
+
+# what can we do with lists?
+# 1. create a list
+# 2. access items in a list - index based access
+# 2a. - check for existence of an item in a list - membership operator
+# 3. modify items in a list - mutable
+# 4. add items to a list - dynamic size / resizable
+# 5. remove items from a list - dynamic size / resizable
+# 6. sort a list
+# 7. reverse a list
+# 8. iterate over a list - loop through a list
+# 9. list comprehension - create a list from another list
+# 10. other list methods - built in functions to work with lists
+# 11. nested lists - list of lists
+# 12. list slicing - get a sublist from a list
