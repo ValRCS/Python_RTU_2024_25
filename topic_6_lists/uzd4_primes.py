a90c65df941a7712bea556b60e68338f8d0f6f64
Valdis Saulespurens <valdis.s.coding@gmail.com>
2025-04-14 18:27:22 +0300

    added flag version of primes

--- topic_6_lists/uzd4_primes.py
+++ topic_6_lists/uzd4_primes.py
@@ -4,17 +4,32 @@
 # TODO add input validation
 cik = int(input("Cik pirmos pirmskaitļus vēlaties atrast? "))
  
-pirmskaitli = []
-skaitlis = 2
+# pirmskaitli = []
+# skaitlis = 2
  
-while len(pirmskaitli) < cik:  # outer loop
-    for i in range(2, int(skaitlis ** 0.5) + 1):
-        if skaitlis % i == 0:
-            break
-    else: # this else is part of for loop
-        # it will run IF the for loop is NOT broken
-        pirmskaitli.append(skaitlis)
-        
-    skaitlis += 1
+# while len(pirmskaitli) < cik:  # outer loop
+#     for i in range(2, int(skaitlis ** 0.5) + 1):
+#         if skaitlis % i == 0:
+#             break
+#     else: # this else is part of for loop
+#         # it will run IF the for loop is NOT broken
+#         pirmskaitli.append(skaitlis)
 
-print(pirmskaitli)
+#     skaitlis += 1
+
+prime_list = [2]
+prime_number = 3   #mazākais pirmskaitlis ?
+
+while len(prime_list) < cik:
+    is_prime = True
+    for i in range(2, int(prime_number ** 0.5) + 1):
+        if prime_number % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        prime_list.append(prime_number)
+    prime_number += 2 # only check odd numbers   
+
+print(f"Pirmie {cik} pirmskaitļi:", prime_list)
+
+# print(prime_list)
