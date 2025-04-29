e5480e3c1b560a067432738ad1e6a61595da49cf
Valdis Saulespurens <valdis.s.coding@gmail.com>
2025-04-29 19:01:04 +0300

    how to install tqdm

--- topic_13_pip_pypi_visualizations/test_tqdm.py
+++ topic_13_pip_pypi_visualizations/test_tqdm.py
@@ -1,6 +1,8 @@
 # let's test our newly installed tqdm package
 # tqdm is a package that allows you to create progress bars
 # it is an external package that is not included in the standard library
+# it is not required but it might be a good idea first to create a virtual environment
+# and then install tqdm in that virtual environment
 
 try:
     from tqdm import tqdm
